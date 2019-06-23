55195f4e90b1fea5470e86a726436a9769ca3900
JJ <jj.orozcolopez.13@gmail.com>
2019-06-23 11:42:28 -0600

    Creación de Controlador y Vistas
    - Se creó controlador Estudiante y sus vistas.
    - Corrección de error respecto a nombre del ensamblado y la carga del type ContextoUniversidad.

---
+++ Universidad-ISI/Controllers/EstudianteController.cs
@@ -0,0 +1,124 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using Universidad_ISI.Datos;
+using Universidad_ISI.Models;
+
+namespace Universidad_ISI.Controllers
+{
+    public class EstudianteController : Controller
+    {
+        private ContextoUniversidad db = new ContextoUniversidad();
+
+        // GET: Estudiante
+        public ActionResult Index()
+        {
+            return View(db.Estudiantes.ToList());
+        }
+
+        // GET: Estudiante/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Estudiante estudiante = db.Estudiantes.Find(id);
+            if (estudiante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(estudiante);
+        }
+
+        // GET: Estudiante/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Estudiante/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EstudianteId,Apellidos,Nombres,FechaInscripcion")] Estudiante estudiante)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Estudiantes.Add(estudiante);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(estudiante);
+        }
+
+        // GET: Estudiante/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Estudiante estudiante = db.Estudiantes.Find(id);
+            if (estudiante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(estudiante);
+        }
+
+        // POST: Estudiante/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EstudianteId,Apellidos,Nombres,FechaInscripcion")] Estudiante estudiante)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(estudiante).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(estudiante);
+        }
+
+        // GET: Estudiante/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Estudiante estudiante = db.Estudiantes.Find(id);
+            if (estudiante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(estudiante);
+        }
+
+        // POST: Estudiante/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Estudiante estudiante = db.Estudiantes.Find(id);
+            db.Estudiantes.Remove(estudiante);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
