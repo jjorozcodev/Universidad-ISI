b080a1050d2483071f66f4c736d72d305ddcda35
JJ <jj.orozcolopez.13@gmail.com>
2019-06-23 11:28:08 -0600

    Preparando Contexto e Inicializador de BD
    Se creó la clase contexto  e inicializador de la base de datos por medio de EF.
    Se configuró el archivo Web.Config para que EF pueda hacer la migración de la BD.

---
+++ Universidad-ISI/Datos/ContextoUniversidad.cs
@@ -0,0 +1,24 @@
+﻿using Universidad_ISI.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Universidad_ISI.Datos
+{
+    public class ContextoUniversidad : DbContext
+    {
+        public ContextoUniversidad() : base("ContextoUniversidad")
+        {
+        }
+
+        public DbSet<Estudiante> Estudiantes { get; set; }
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Inscripcion> Inscripciones { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ Universidad-ISI/Datos/InicializadorBD.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Universidad_ISI.Models;
+using System.Data.Entity;
+
+namespace Universidad_ISI.Datos
+{
+    public class InicializadorBD : DropCreateDatabaseIfModelChanges<ContextoUniversidad>
+    {
+        protected override void Seed(ContextoUniversidad context)
+        {
+            //base.Seed(context);
+
+            var Estudiantes = new List<Estudiante>
+            {
+                new Estudiante{Nombres="Carson", Apellidos="Alexander", FechaInscripcion=DateTime.Parse("2005-09-01")},
+                new Estudiante{Nombres="Meredith", Apellidos="Alonso", FechaInscripcion=DateTime.Parse("2002-09-01")},
+                new Estudiante{Nombres="Arturo", Apellidos="Anand", FechaInscripcion=DateTime.Parse("2003-09-01")},
+                new Estudiante{Nombres="Gytis", Apellidos="Barzdukas", FechaInscripcion=DateTime.Parse("2002-09-01")},
+                new Estudiante{Nombres="Yan", Apellidos="Li", FechaInscripcion=DateTime.Parse("2002-09-01")},
+                new Estudiante{Nombres="Peggy", Apellidos="Justice", FechaInscripcion=DateTime.Parse("2001-09-01")},
+                new Estudiante{Nombres="Laura", Apellidos="Norman", FechaInscripcion=DateTime.Parse("2003-09-01")},
+                new Estudiante{Nombres="Nino", Apellidos="Olivetto", FechaInscripcion=DateTime.Parse("2005-09-01")}
+            };
+
+            Estudiantes.ForEach(e => context.Estudiantes.Add(e));
+
+            context.SaveChanges();
+
+            var Cursos = new List<Curso>
+            {
+                new Curso{ CursoId=1050, Titulo="Chemistry", Creditos=3,},
+                new Curso{ CursoId=4022, Titulo="Microeconomics", Creditos=3,},
+                new Curso{ CursoId=4041, Titulo="Macroeconomics", Creditos=3,},
+                new Curso{ CursoId=1045, Titulo="Calculus", Creditos=4,},
+                new Curso{ CursoId=3141, Titulo="Trigonometry", Creditos=4,},
+                new Curso{ CursoId=2021, Titulo="Composition", Creditos=3,},
+                new Curso{ CursoId=2042, Titulo="Literature", Creditos=4,}
+            };
+
+            Cursos.ForEach(c => context.Cursos.Add(c));
+
+            context.SaveChanges();
+
+            var Inscripciones = new List<Inscripcion>
+            {
+                new Inscripcion{ EstudianteId=1, CursoId=1050, Nota=Nota.A},
+                new Inscripcion{ EstudianteId=1, CursoId=4022, Nota=Nota.C},
+                new Inscripcion{ EstudianteId=1, CursoId=4041, Nota=Nota.B},
+                new Inscripcion{ EstudianteId=2, CursoId=1045, Nota=Nota.B},
+                new Inscripcion{ EstudianteId=2, CursoId=3141, Nota=Nota.F},
+                new Inscripcion{ EstudianteId=2, CursoId=2021, Nota=Nota.F},
+                new Inscripcion{ EstudianteId=3, CursoId=1050,},
+                new Inscripcion{ EstudianteId=4, CursoId=1050,},
+                new Inscripcion{ EstudianteId=4, CursoId=4022, Nota=Nota.F},
+                new Inscripcion{ EstudianteId=5, CursoId=4041, Nota=Nota.C},
+                new Inscripcion{ EstudianteId=6, CursoId=1045},
+                new Inscripcion{ EstudianteId=7, CursoId=3141, Nota=Nota.A},
+            };
+
+            Inscripciones.ForEach(i => context.Inscripciones.Add(i));
+
+            context.SaveChanges();
+        }
+    }
+}
